1bbe75f6ceed4603e2a70eadb8f7020035cd9561
thisisazeez <thisisazeez0@gmail.com>
2023-08-11 16:00:21 +0100

    project setup

---
+++ devkit/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from devkit.users.models import User
+from devkit.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
